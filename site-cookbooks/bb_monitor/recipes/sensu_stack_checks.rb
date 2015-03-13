6dc4c24a7aa97ead95025734c18bb28aa72e33de
Stephen Brandon <brandon.steve@gmail.com>
2015-03-13 18:06:11 +0100

    remove elb checks

--- site-cookbooks/bb_monitor/recipes/sensu_stack_checks.rb
+++ site-cookbooks/bb_monitor/recipes/sensu_stack_checks.rb
@@ -10,27 +10,6 @@
   end
 end
 
-node[:opsworks][:layers].each do |key, value|
-  value["elb-load-balancers"].each do |elb|
-    sensu_check "metric-sensu-elb-#{elb[:name]}" do
-      type "metric"
-      command "elb-metrics.rb -r #{node[:aws_region]} -n #{elb[:name]} --scheme stats.#{node[:opsworks][:stack][:name].downcase.gsub(' ','_')}.#{key}.elb"
-      handlers node[:bb_monitor][:sensu][:default_metric_handlers]
-      subscribers ["dashboard"]
-      interval 150
-    end
-
-    sensu_check "check-sensu-elb-#{elb[:name]}" do
-      command "check-elb-health.rb -r #{node[:aws_region]} -n #{elb[:name]} -v"
-      handlers node[:bb_monitor][:sensu][:default_check_handlers]
-      subscribers ["dashboard"]
-      interval 150
-      additional(:occurrences => 2)
-    end
-
-  end
-end
-
 sensu_check "check-graphite-cache" do
   command "graphite.rb -h #{node[:graphite][:host]}:8081 -t carbon.agents.*.cache.size -a 1000,5000 -p 10minutes -g"
   handlers node[:bb_monitor][:sensu][:default_check_handlers]
